3b1aec8efbf46aded22cd9c8c6ce6a87b0db1466
Maxim Manuylov <maxim.manuylov@gmail.com>
2015-07-17 16:04:04 +0200

    Update Main.java

--- src/com/mycompany/Main.java
+++ src/com/mycompany/Main.java
@@ -2,6 +2,6 @@ package com.mycompany;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    System.out.println("Hello, world!"); // comment
   }
 }
